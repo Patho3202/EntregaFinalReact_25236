cec9f5e4d2674fb3d5150f82b7b57aa899f78ce8
Patho3202 <patorahal@gmail.com>
2025-12-02 23:42:01 -0300

    Corrijo importación de Dashboard.css

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useAuthContext } from '../context/AuthContext';
 import { Link, useNavigate } from 'react-router-dom';
-import '../pages/dashboard.css'; // Import del CSS
+import '../pages/Dashboard.css'; // Import del CSS
 
 export default function Dashboard() {
   const { usuario, cerrarSesion } = useAuthContext();
